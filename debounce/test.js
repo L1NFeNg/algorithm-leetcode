6307d52f1bcbb24e1889c6e6ec76abf2c400c1e5
lingfeng <332373135@qq.com>
2024-03-13 18:36:00 +0800

    add test

---
+++ debounce/test.js
@@ -0,0 +1,13 @@
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    const context = this;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
