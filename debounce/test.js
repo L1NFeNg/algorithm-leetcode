c5a9c2121ac89e69d0d52d40907acdcaed6e14db
lingfeng <332373135@qq.com>
2024-03-14 14:30:11 +0800

    联系

--- debounce/test.js
+++ debounce/test.js
@@ -1,22 +1,9 @@
 function debounce(func, delay) {
   let timeoutId;
-  const context = this;
-
-  return function (...args) {
-    console.log(context);
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-
+  return function (...arg) {
+    if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
-      func.apply(context, args);
+      func.apply(this, arg);
     }, delay);
   };
 }
-
-function myFunction() {
-  console.log("函数被调用了");
-}
-
-const debouncedFunction = debounce(myFunction, 3000); // 创建一个防抖处理后的函数
-debouncedFunction(); // 函数调用不会立即执行
